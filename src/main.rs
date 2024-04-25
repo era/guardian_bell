2315c6d249888c76176b8951e0313a20db743867
era <me@elias.sh>
2024-04-26 08:30:35 +0900

    moving code to internal mods

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,21 @@
-use tonic_health::server::HealthReporter;
 use tonic::{transport::Server, Request, Response, Status};
-use proto::ingestion_server::{Ingestion, IngestionServer};
-use proto::{PutRequest, PutResponse};
+use tonic_health::server::HealthReporter;
 
-pub mod proto {
-    tonic::include_proto!("metrics_service");
-}
-
-#[derive(Default)]
-pub struct MetricsService {}
-
-
-#[tonic::async_trait]
-impl Ingestion for MetricsService {
-    async fn put(&self, req: Request<PutRequest>) -> Result<Response<PutResponse>, Status> {
-        Ok(Response::new(PutResponse{}))
-    }
-}
+mod grpc;
+mod metrics;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (mut health_reporter, health_service) = tonic_health::server::health_reporter();
-    health_reporter.set_serving::<IngestionServer<MetricsService>>().await; // set_not_serving for unhealthy
 
     let addr = "[::1]:50051".parse().unwrap();
-    let metrics_service = MetricsService::default();
+    let metrics_service = metrics::server::MetricsService::new(health_reporter.clone());
 
     Server::builder()
         .add_service(health_service)
-        .add_service(IngestionServer::new(metrics_service))
+        .add_service(metrics_service.to_grpc().await)
         .serve(addr)
         .await?;
-    
+
     Ok(())
 }
