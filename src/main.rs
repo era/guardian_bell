ade0cc6210d0d5c8ad3ee1e1bba49a895ec704ef
era <me@elias.sh>
2024-04-26 18:34:30 +0900

    adding clap and refactoring things out of main

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,19 @@
-use tonic::{transport::Server};
+use clap::Parser;
 
-
-mod grpc;
+mod app;
 mod metrics;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    grpc_server_port: u16,
+    #[arg(short, long)]
+    log_path: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let (health_reporter, health_service) = tonic_health::server::health_reporter();
-
-    let addr = "[::1]:50051".parse().unwrap();
-    let metrics_service = metrics::server::MetricsService::new(health_reporter.clone());
-
-    Server::builder()
-        .add_service(health_service)
-        .add_service(metrics_service.ingestion_server().await)
-        .serve(addr)
-        .await?;
-
-    Ok(())
+    let args = Args::parse();
+    app::App::run_server(args.grpc_server_port, &args.log_path).await
 }
--- src/metrics/server.rs
+++ src/metrics/server.rs
@@ -2,11 +2,13 @@ use proto::ingestion_server::{Ingestion, IngestionServer};
 use proto::{PutRequest, PutResponse};
 use tonic::{Request, Response, Status};
 use tonic_health::server::HealthReporter;
+use tracing::instrument;
 
 pub mod proto {
     tonic::include_proto!("metrics_service");
 }
 
+#[derive(Clone, Debug)]
 pub struct MetricsService {
     health_reporter: HealthReporter,
 }
@@ -16,17 +18,18 @@ impl MetricsService {
         Self { health_reporter }
     }
 
-    pub async fn ingestion_server(mut self) -> IngestionServer<MetricsService> {
+    pub async fn ingestion_server(&mut self) -> IngestionServer<MetricsService> {
         //FIXME
         self.health_reporter
             .set_serving::<IngestionServer<MetricsService>>()
             .await; // set_not_serving for unhealthy
-        IngestionServer::new(self)
+        IngestionServer::new(self.clone())
     }
 }
 
 #[tonic::async_trait]
 impl Ingestion for MetricsService {
+    #[instrument]
     async fn put(&self, _req: Request<PutRequest>) -> Result<Response<PutResponse>, Status> {
         Ok(Response::new(PutResponse {}))
     }
