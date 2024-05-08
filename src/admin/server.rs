d3d49079b9af9ca93eedd04972203b2a6fde66e2
era <me@elias.sh>
2024-05-09 08:01:34 +0900

    allow server to gracefully shutdown

---
+++ src/admin/server.rs
@@ -0,0 +1,51 @@
+use proto::admin_server::{Admin, AdminServer};
+use proto::{ShutdownRequest, ShutdownResponse};
+use tonic::{Request, Response, Status};
+use tonic_health::server::HealthReporter;
+use tracing::instrument;
+use tokio::sync::mpsc::{Sender};
+use crate::server;
+
+pub mod proto {
+    tonic::include_proto!("admin_service");
+}
+
+#[derive(Clone, Debug)]
+pub struct AdminService {
+    health_reporter: HealthReporter,
+    tx: Sender<bool>
+}
+
+impl AdminService {
+    pub fn new(health_reporter: HealthReporter, tx: Sender<bool>) -> Self {
+        Self {health_reporter, tx}
+    }
+
+    pub async fn admin_server(&mut self) -> AdminServer<AdminService> {
+        //FIXME
+        self.health_reporter
+            .set_serving::<AdminServer<AdminService>>()
+            .await; // set_not_serving for unhealthy
+        AdminServer::new(self.clone())
+    }
+}
+
+#[tonic::async_trait]
+impl server::Administrable for AdminService {
+    async fn shutdown(&mut self) -> Result<(), server::ShutdownError> {
+        let mut health_reporter = self.health_reporter.clone();
+        health_reporter.set_not_serving::<AdminServer<AdminService>>().await;
+
+        Ok(())
+    }
+}
+
+#[tonic::async_trait]
+impl Admin for AdminService {
+
+    #[instrument]
+    async fn shutdown(&self, _req: Request<ShutdownRequest>) -> Result<Response<ShutdownResponse>, Status> {
+        self.tx.send(true).await.unwrap(); //FIXME
+        Ok(Response::new(ShutdownResponse {}))
+    }
+}
---
+++ src/server.rs
@@ -0,0 +1,12 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum ShutdownError {
+    #[error("unkown cause {0}")]
+    UnknownCause(String)
+}
+
+#[tonic::async_trait]
+pub trait Administrable {
+    async fn shutdown(&mut self) -> Result<(), ShutdownError>;
+}
