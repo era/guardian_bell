917028287c2f2414d4e31cb1e6196a6040a0fa7d
era <me@elias.sh>
2024-06-02 20:02:03 +0900

    writing wal api

--- src/wal/mod.rs
+++ src/wal/mod.rs
@@ -8,14 +8,18 @@ pub enum Error {
     IOError(#[from] StdIOError),
 }
 
-/// RecoverableLog is basically a WAL, it allow callers to write to the end to the file
-/// and read from any position. If there is nothing to be read, it returns None
-pub trait RecoverableLog {
-    /// write data to the end of the file
+pub trait WriteAheadLog {
     fn write(&mut self, data: &Vec<u8>) -> Result<(), Error>;
-    fn read<R>(&self, offset: u64, buf: &mut BufReader<R>, lines: u64) -> Result<Option<u64>, Error>;
+    fn read<R>(
+        &self,
+        offset: u64,
+        buf: &mut BufReader<R>,
+        lines: u64,
+    ) -> Result<Option<u64>, Error>;
 }
 
+/// Log is basically a single-file WAL, it allow callers to write to the end of the file
+/// and read R bytes from any offset. If there is nothing to be read, it returns None
 struct Log {
     file: File,
     name: String,
@@ -30,25 +34,58 @@ impl Log {
 
         Ok(Self { file, name })
     }
-}
 
-impl RecoverableLog for Log {
+    /// write data to the end of the file
     fn write(&mut self, data: &Vec<u8>) -> Result<(), Error> {
         self.file.write_all(data)?;
         Ok(())
     }
-    fn read<R>(&self, offset: u64, buf: &mut BufReader<R>, lines: u64) -> Result<Option<u64>, Error> {
+
+    /// R is the size of the entries on the log
+    fn read<R>(
+        &self,
+        offset: u64,
+        buf: &mut BufReader<R>,
+        lines: u64,
+    ) -> Result<Option<u64>, Error> {
         todo!()
     }
 }
 
+/// Implements a WAL for the application
+/// is up to the caller to handle snapshot (to reduce the disk usage, probably
+/// should create a new WAL dump the content from memory and deleting the old one), validating
+/// the content of the files (e.g. making sure all entries are valid and not corrupted)
+/// and handling any indexing needed.
 pub struct WAL {
     path: PathBuf,
     logs: Vec<Log>,
+    page: u32,
 }
 
 impl WAL {
     fn new(path: PathBuf) -> Self {
-        Self { path, logs: vec![] }
+        //TODO open logs
+        Self {
+            path,
+            logs: vec![],
+            page: 0,
+        }
+    }
+
+    /// writes to the end of the last page
+    fn write(&mut self, data: &Vec<u8>) -> Result<(), Error> {
+        todo!()
+    }
+
+    /// read page starting from offset. Each page is a log file.
+    fn read<R>(
+        &self,
+        page: u32,
+        offset: u64,
+        buf: &mut BufReader<R>,
+        lines: u64,
+    ) -> Result<Option<u64>, Error> {
+        todo!()
     }
 }
